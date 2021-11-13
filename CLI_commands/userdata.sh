20c6aa83ad34827d054da53f69672d43a65331a3
kocagozhkn <kocagoz@gmail.com>
2021-11-13 21:07:51 +0300

    userdata.sh revised

---
+++ CLI_commands/userdata.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+cd /home/ec2-user
+wget https://raw.githubusercontent.com/kocagozhkn/AWS_Project_01-Roman-Numerals-Converter/main/app.py
+mkdir templates
+cd templates
+wget https://raw.githubusercontent.com/kocagozhkn/AWS_Project_01-Roman-Numerals-Converter/main/templates/index.html
+wget https://raw.githubusercontent.com/kocagozhkn/AWS_Project_01-Roman-Numerals-Converter/main/templates/result.html
+cd ..
+python3 app.py
